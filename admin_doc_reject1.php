29d6b9d644af0ee42e053d4d074302a590167459
albinaj <albinjosephaj5@gmail.com>
2020-01-21 10:40:16 +0530

    php_second_project

---
+++ admin_doc_reject1.php
@@ -0,0 +1,11 @@
+<?php
+include('connection.php');
+$var = $_GET['id'];
+$sql="UPDATE doctor_registration SET status = 'Removed' where doctor_id = '".$var."'";
+    if (mysqli_query($conn,$sql))
+        {
+            header('location:admin_doctors.php');
+        }
+
+mysqli_close($conn);
+?>
